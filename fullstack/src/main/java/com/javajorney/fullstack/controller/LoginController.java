47b05d988d4702ca985cd3ecb79f4a519d067bc7
Romualdo <rregorr@gmail.com>
2022-02-03 11:28:34 -0300

    commitando

--- fullstack/src/main/java/com/javajorney/fullstack/controller/LoginController.java
+++ fullstack/src/main/java/com/javajorney/fullstack/controller/LoginController.java
@@ -3,14 +3,13 @@ package com.javajorney.fullstack.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/")
-public class HomeController {
-    @ResponseBody
+@RequestMapping("/login")
+public class LoginController {
+
     @GetMapping
-    public String hello() {
-       return "Ola, mundaoooo450!";
+    public String login() {
+        return "login";
     }
 }
