76c47f60344c272a35a9420f17af92f8aab9dd98
Romualdo <rregorr@gmail.com>
2022-02-04 22:39:11 -0300

    Enviando

---
+++ fullstack/src/main/java/com/javajorney/fullstack/model/Autentica.java
@@ -0,0 +1,31 @@
+package com.javajorney.fullstack.model;
+
+public class Autentica {
+    private String usuario;
+    private String senha;
+    private String email;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
