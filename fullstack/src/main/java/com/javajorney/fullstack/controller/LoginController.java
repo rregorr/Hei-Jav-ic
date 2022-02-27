e5c859c9e460fd60fa1487afad38e11792c9b907
Romualdo <rregorr@gmail.com>
2022-02-27 15:17:26 -0300

    e

--- fullstack/src/main/java/com/javajorney/fullstack/controller/LoginController.java
+++ fullstack/src/main/java/com/javajorney/fullstack/controller/LoginController.java
@@ -1,6 +1,10 @@
 package com.javajorney.fullstack.controller;
 
-import com.javajorney.fullstack.model.Autentica;
+import com.javajorney.fullstack.model.*;
+import com.javajorney.fullstack.repository.AnoRepository;
+import com.javajorney.fullstack.repository.BancaRepository;
+import com.javajorney.fullstack.repository.DisciplinaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +18,12 @@ import java.util.List;
 @Controller
 @RequestMapping("/login")//corresponde à URI do action em login.html. Nomes devem ser iguais.
 public class LoginController {
+    @Autowired//injeta uma instância de DisciplinaRepository em nosso Controller.
+    DisciplinaRepository disciplinaRepository;
+    @Autowired//injeta uma instância de DisciplinaRepository em nosso Controller.
+    BancaRepository bancaRepository;
+    @Autowired//injeta uma instância de DisciplinaRepository em nosso Controller.
+    AnoRepository anoRepository;
 
     @GetMapping//("/algumacoisa")
     public String login() {
@@ -34,15 +44,37 @@ public class LoginController {
 
         List<Autentica> usuarios = Arrays.asList(usuario1,usuario2);
 
+        List<Disciplina> disciplinas = disciplinaRepository.findAll();
+        model.addAttribute("disciplinas", disciplinas);
+        List<Banca> bancas = bancaRepository.findAll();
+        model.addAttribute("bancas", bancas);
+        List<Ano> anos = anoRepository.findAll();
+        model.addAttribute("anos", anos);
+
+        //Aqui estamos criando uma lista de disciplinas e já populando ela com os dados de disciplinas vindos
+        // do DB através do método findAll. Após isso, estamos usando a instância model que o thymeleaf
+        // nos retorna no parâmetro de nosso método que é uma forma de passar dados do Controller para a view.
+        // Como pode ver, estamos passando para a view a lista de disciplinas que acabamos de popular e colocamos
+        // o nome "disciplinas" como chave para acessar esses dados na view. Poderíamos ter colocado qualquer
+        // nome para essa chave, mas é claro que quanto mais coerente, melhor.
+        // model.addAttribute("disciplinas", disciplinas); o primeiro parâmetro é a chave que usaremos para
+        // recuperar os dados que estamos enviando para a View e o segundo parâmetro se refere ao que estamos
+        // enviando em si.Pronto, a view já irá receber esses dados das disciplinas assim que fizermos o nosso
+        // Login e entrarmos na página Home. :)
+
+        model.addAttribute("questao", new Questao());
+
+
         for (int i = 0; i < usuarios.size(); i++) {
-            if (autentica.getEmail().equals(usuarios.get(i).getEmail()) && autentica.getSenha().equals(usuarios.get(i).getSenha())) {
-                return "cadastro";//em caso de sucesso, ir para pagina inicial
+            if (autentica.getEmail().equals(usuarios.get(i).getEmail()) && autentica.getSenha().equals(usuarios.get
+                    (i).getSenha())) {
+                return "resolucao";//em caso de sucesso, ir para pagina inicial
             }
         }
         model.addAttribute("loginError", true);
 
         return "login"; //em caso de erro, retornar à pagina de login
 
-    }//diz à view que o objeto cujos
-    //parametros é autentica é igual à view. mapeia autentica da view com autentica do controller.
+    }//diz à view que o objeto cujos parametros é autentica é igual à view. mapeia autentica da view com autentica do controller.
+    //Aplique esse código e você já terá as disciplinas à disposição na View.
 }
