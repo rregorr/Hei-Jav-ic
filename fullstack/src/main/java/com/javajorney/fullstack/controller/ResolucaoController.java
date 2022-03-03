79c0e623a11aff69b207ab9b8a7821e3d08980c3
Romualdo <rregorr@gmail.com>
2022-03-02 21:59:44 -0300

    e

--- fullstack/src/main/java/com/javajorney/fullstack/controller/ResolucaoController.java
+++ fullstack/src/main/java/com/javajorney/fullstack/controller/ResolucaoController.java
@@ -1,10 +1,13 @@
 package com.javajorney.fullstack.controller;
 
+import com.javajorney.fullstack.model.Ano;
+import com.javajorney.fullstack.model.Banca;
 import com.javajorney.fullstack.model.Disciplina;
 import com.javajorney.fullstack.model.Questao;
 import com.javajorney.fullstack.repository.AnoRepository;
 import com.javajorney.fullstack.repository.BancaRepository;
 import com.javajorney.fullstack.repository.DisciplinaRepository;
+import com.javajorney.fullstack.repository.QuestaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
+import static sun.jvm.hotspot.code.CompressedStream.L;
+
 @Controller
 @RequestMapping("/resolver")
 public class ResolucaoController {
@@ -24,15 +29,20 @@ public class ResolucaoController {
     BancaRepository bancaRepository;
     @Autowired//injeta uma instância de DisciplinaRepository em nosso Controller.
     AnoRepository anoRepository;
+    @Autowired
+    QuestaoRepository questaoRepository;
 
     @RequestMapping
     public String resolver (Model model) {
         List<Disciplina>  disciplina  = disciplinaRepository.findAll(); //busca a lista de disciplinas no banco
-        model.addAttribute("disciplina", disciplina); //associa a entidade disciplina do banco à propriedade disciplina, da view
-        /*bancas = bancaRepository.findAll();
-        model.addAttribute("bancas", bancas);
-        anos = anoRepository.findAll();
-        model.addAttribute("anos", anos);*/
+        model.addAttribute("disciplina", disciplina); //associa o objeto disciplina, com os dados
+        //das disciplinas que vieram do banco, à propriedade "disciplina", definida na view.
+        List<Banca> banca = bancaRepository.findAll();
+        model.addAttribute("banca", banca);
+        List<Ano> ano = anoRepository.findAll();
+        model.addAttribute("ano", ano);
+
+        Questao questao = questaoRepository.findById(Long.(1));
 
         return "resolucao";
         }
